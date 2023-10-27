7d2cbeaa7a98ded8a10f7e896cc82a8b0c936f26
Adriana <zolotcovadriana@gmail.com>
2023-10-27 19:55:47 +0300

    first commit

---
+++ src/main/java/com/example/proiectdean/Controller/controllerVote.java
@@ -0,0 +1,20 @@
+package com.example.proiectdean.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controllerVote {
+    String data= "eu sunt de aici";
+    @GetMapping("/getCandidates")
+    public Candidates[] getCandidates(){
+        return Candidates.getAll();
+    }
+
+//    @PostMapping
+//    public User saveUser(@Validated @RequestBody User user) {
+//        // Implement
+//    }
+
+}
---
+++ src/test/java/com/example/proiectdean/ProiectDeAnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proiectdean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectDeAnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
